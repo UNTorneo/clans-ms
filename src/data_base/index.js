ae8dc11f44d83234208ea27a343c9838cafc1fa9
jubedoyat <jubedoyat@unal.edu.co>
2023-03-04 18:06:27 -0500

    it is possible to make CRUD ops

--- src/data_base/index.js
+++ src/data_base/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const db = require('./queries')
 const app = express();
 const port = 3000;
 
@@ -13,6 +14,12 @@ app.use(
 app.get('/', (request, response) => {
   response.json({ info: 'Node.js, Express, and Postgres API' })
 });
+
+app.get('/clans', db.getClans)
+app.get('/clans/:id', db.getClanById)
+app.post('/clans', db.createClan)
+app.put('/clans/:id', db.updateClan)
+app.delete('/clans/:id', db.deleteClan)
 
 app.listen(port, () => {
   console.log(`App running on port ${port}.`)
--- src/data_base/queries.js
+++ src/data_base/queries.js
@@ -1,4 +1,7 @@
-const Pool = require('pg').Pool
+const { request, response } = require('express');
+
+const Pool = require('pg').Pool;
+
 const pool = new Pool({
   user: 'admin',
   host: 'localhost',
@@ -6,3 +9,71 @@ const pool = new Pool({
   password: 'bedoya2501',
   port: 5432,
 });
+
+const getClans = (request, response) => {
+    pool.query('SELECT * FROM clans ORDER BY id ASC', (error, results) => {
+        if (error) {
+            throw error;
+        }
+        response.status(200).json(results.rows);
+    });
+}
+
+const getClanById = (request, response) => {
+    const id = parseInt(request.params.id);
+
+    pool.query('SELECT * FROM clans WHERE id = $1', [id], (error, results) =>{
+        if (eror) {
+            throw error;
+        }
+        response.status(200).json(results.rows);
+    })
+}
+
+const createClan = (request, response) => {
+  const name = request.body;
+  const date = new Date;
+  const created_at = date.setFullYear.toString + date.getMonth.toString + date.getDay.toString;
+
+  pool.query('INSERT INTO clans (name, created_at) VALUES ($1, $2) RETURNING *', [name, created_at], (error, results) => {
+    if (error) {
+      throw error;
+    }
+    response.status(201).send(`User added with ID: ${results.rows[0].id}`)
+  });
+}
+
+const updateClan = (request, response) => {
+  const id = parseInt(request.params.id);
+  const name = request.body;
+
+  pool.query(
+    'UPDATE clans SET name = $1 WHERE id = $3',
+    [name, id],
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      response.status(200).send(`User modified with ID: ${id}`);
+    }
+  );
+}
+
+const deleteClan = (request, response) => {
+    const id = parseInt(request.params.id);
+
+    pool.query('DELETE FROM clans WHERE id = $1', [id], (error, results) => {
+        if (error) {
+            throw error;
+        }
+        response.status(200).send(`Clan deleted with id = ${id}`);
+    });
+}
+
+module.exports = {
+    getClanById,
+    getClans,
+    createClan,
+    updateClan,
+    deleteClan
+};
