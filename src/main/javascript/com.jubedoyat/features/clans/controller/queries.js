1c0573f35907b2ed13a14d960473cc18d7fb0a01
jubedoyat <jubedoyat@unal.edu.co>
2023-05-08 03:00:24 -0500

    addUserToClan communication with users-ms complete

--- src/main/javascript/com.jubedoyat/features/clans/controller/queries.js
+++ src/main/javascript/com.jubedoyat/features/clans/controller/queries.js
@@ -2,6 +2,17 @@ const { request, response } = require('express');
 const { Sequelize } = require('sequelize');
 const { Clan } = require('../models/clan_models');
 const { UsersClan } = require('../models/users_clan_model');
+const axios = require('axios');
+
+async function getUserById(userId) {
+  try {
+    const response = await axios.get(`http://localhost:8088/users/${userId}`);
+    return response.data;
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
+}
 
 const getClanById = async (request, response) => {
   const id = parseInt(request.params.id);
@@ -93,18 +104,24 @@ const deleteClan = async (request, response) => {
 const getUsersByClanId = async (request, response) => {
   const clanIdToFind = parseInt(request.params.id);
   try {
-    const users = await UsersClan.findAll({
+    const res = await UsersClan.findAll({
       where: {'clanId' : clanIdToFind}
     });
-    if (users == null) {
+    if (res == null) {
       response.json({
         'message' : 'Not found'
       });
     }
-    users.forEach((user) => delete user.dataValues.clanId);
-    response.json(users);
-    console.log(users.every(user => user instanceof UsersClan));
+    const users = res.map((e) => e.dataValues['userId']);
+    const usersFromServer = users.map((userId) => getUserById(userId)
+      .then(userData => {
+        return userData;
+      })
+    );
+    console.log(usersFromServer);
+    response.json(usersFromServer);
   } catch (error) {
+    console.log(error);
     response.json({
         'error' : error
       });
@@ -114,23 +131,30 @@ const getUsersByClanId = async (request, response) => {
 const addUserToClan = async (request, response) => {
   const clanId = parseInt(request.params.id);
   const userId = request.body['userId'];
-  console.log(userId);
   const isClanIdCorrect = Clan.findAll({where: {id : clanId}}) != null;
-  if (isClanIdCorrect) {
-    try {
-      await UsersClan.create({
-        'clanId' : clanId,
-        'userId' : userId
-      });
-      response.json({
-        'message' : `User with id ${userId} added to clan with id ${clanId}`
-      });
-    } catch (error) {
+  getUserById(userId)
+    .then(async userData => {
+      if (isClanIdCorrect) {
+        try {
+          await UsersClan.create({
+            'clanId' : clanId,
+            'userId' : userId
+          });
+          response.json({
+            'message' : `User with id ${userId} added to clan with id ${clanId}`
+          });
+        } catch (error) {
+          console.log(error);
+          response.json(error);
+        }
+      }
+    })
+    .catch(error => {
+      console.log(error);
       response.json({
         'error' : error
-      });
-    }
-  }
+      })
+    });
 }
 
 module.exports = {
